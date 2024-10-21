7b178500c303f5bb7f6b1f031161d17692e81c62
mchyla7 <mchyla7@gmail.com>
2024-10-21 09:09:02 +0200

    ➕ Přidán program na SOUČUIN

---
+++ Fromulare/SoucetRozdilSoucinPodil.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p>Zadejte dvě čísla: </p>
+        <form method="POST" action = "">
+            <input name="cislo1" type="text" /> <br />
+            <input name="cislo2" type="text" /> <br />
+            <input type="submit" value="Sečti" />
+        </form>
+        <?php
+        // put your code here
+        
+        if ($_POST)
+        {
+            $c1 = $_POST['cislo1'];
+            $c2 = $_POST['cislo2'];
+            
+            if (isset($c1) && is_numeric($c1) && isset($c2) && is_numeric($c2))
+                {
+                $soucet = $c1 + $c2;
+                $rozdil = $c1 - $c2;
+                $soucin = $c1 * $c2;
+                $podil = $c1 / $c2;
+                
+                echo("Součet: $soucet\n");
+                echo("Rozdíl: $rozdil\n");
+                echo("Součin: $soucin\n");
+                echo("Podíl: $podil\n");
+                }
+            else
+            {
+                echo("Zadej číselnou hodnotu");
+            }
+        }
+        ?>
+    </body>
+</html>
