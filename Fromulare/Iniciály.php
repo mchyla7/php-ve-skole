3262c01d164ede10bff75f5ebce572aa42265ae5
mchyla7 <mchyla7@gmail.com>
2024-11-11 09:40:08 +0100

    ➕ Přidán update

--- Fromulare/Iniciály.php
+++ Fromulare/Iniciály.php
@@ -10,8 +10,31 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <form method="POST" action = "">
+        <p>Tvoje jméno abych získal iniciály.</p>
+        <label>Zadejte Jméno: </label><input name="jmeno" type="text" /> <br />
+        <label>Zadejte Příjmení: </label><input name="prijmeni" type="text" /> <br />
+            <input type="submit" value="Dělej" />
+        </form>
         <?php
         // put your code here
+        
+        define("NL","<br>\n");
+        if ($_POST)
+        {
+            $jmeno = trim($_POST['jmeno']);
+            $prijmeni = trim($_POST['prijmeni']);
+            
+            if(!empty($jmeno) or !empty($prijmeni)) // Ošetření při prvním volání
+            {
+                echo(mb_strtoupper(mb_strstr($jmeno,0,1).".".mb_subst($prijmeni,0,1)."."));
+                
+            }
+        }
+        else
+        {
+                echo("Zadej text: ");
+        }
         ?>
     </body>
 </html>
--- Fromulare/odstranCisliceAInverze.php
+++ Fromulare/odstranCisliceAInverze.php
@@ -10,8 +10,31 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <form method="POST" action = "">
+        <label>Zadejte text: </label><input name="text" type="text" /> <br />
+            <input type="submit" value="Dělej" />
+        </form>
         <?php
         // put your code here
+        
+        define("NL","<br>\n");
+        if ($_POST)
+        {
+            $text = $_POST['text'];
+
+            
+            if(!empty($text)) // Ošetření při prvním volání
+            {
+                $cislisce = array(0,1,2,3,4,5,6,7,8,9);
+                foreach ($cislisce as $c) $text = str_replace ($c, "", $text);
+                        
+                for ($i=mb_strlen($text)-1;$i>=0;i--)echo($text[$i]);
+                
+            }
+        else
+        {
+                echo("Zadej text: ");
+        }
         ?>
     </body>
 </html>
