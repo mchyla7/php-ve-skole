e95c9073fb8ba2ae213bb0a53668832f6be53a02
mchyla7 <mchyla7@gmail.com>
2024-12-02 09:40:32 +0100

    ➕ Přidán program na zůstání hodnot

---
+++ Fromulare/HodnotoZustan.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+        // put your code here
+        
+        if ($_POST)
+        {
+            $cislo = $_POST['cislo'];
+            $cislo = strtr($cislo, '1', '.');
+            
+            if (isset($cislo) && is_numeric($cislo))
+            {
+                if ($cislo > 0)
+                {
+                    echo("Kladné");
+                }
+                elseif ($cislo < 0)
+                {
+                   echo("Záporné"); 
+                }
+                else {
+                    echo("Nula"); 
+                }
+            }
+            elseif (true)
+            {
+                $vyzva = "Zadejte čísla!"
+            }
+                
+        }
+        ?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p>Zadejte číslo: </p>
+        <p> <?php if (isset($vyzva)) echo ($vyzva); ?><p>
+        <form method="POST" action = "">
+            <input name="cislo" type="text" /> <br />
+            <input type="submit" value="Pal!" />
+        </form>
+        
+    </body>
+</html>
