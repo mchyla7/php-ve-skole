adc661b16d82f06c48e84433e5e425caba6c2d45
mchyla7 <mchyla7@gmail.com>
2025-01-06 09:39:00 +0100

    ➕ Přidán další formSelecet program

---
+++ Fromulare/formSelecstScitam.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form method="post" action="">
+            <select name="operace">
+                <option value="100">100</option>
+                <option value="200">200</option>
+                <option value="300">300</option>
+                <option value="400">400</option>
+                <option value="500">500</option>
+            </select>
+            <select name="operaceSecti">
+                <option value="100">100</option>
+                <option value="200">200</option>
+                <option value="300">300</option>
+                <option value="400">400</option>
+                <option value="500">500</option>
+            </select>
+            <br>
+            <input type="submit" value="Jo?">
+        </form>
+        <?php
+        // put your code here
+        if ($_POST)
+        {
+            $operace = $_POST['operace'];
+            $operaceSecti = $_POST['operaceSecti'];
+            echo("Vybrali jste: ". ($operace + $operaceSecti));
+        }
+        ?>
+    </body>
+</html>
