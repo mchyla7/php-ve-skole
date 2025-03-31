d6740488805bc36afcdcd563be60056fdb7f7141
mchyla7 <mchyla7@gmail.com>
2025-03-31 09:40:00 +0200

    ➕ Upraven dynamicWeb

--- dynamWWW/index.php
+++ dynamWWW/index.php
@@ -4,47 +4,38 @@
     <title></title>
     <meta charset='utf-8'>
     <link rel="stylesheet" href="sipka.css" type="text/css">
-    
   </head>
   <body>
-  <div class="obal">
-    <div class="hlavicka">
-      Toto je hlavička
-    </div> <!--KONEC HLAVIČKA -->
-    
-    <div class="pas">
-    <div class="levy">
-           <?php  require("menu.php");   ?>
-    </div> <!-- KONEC LEVY-->
-    
-    
-          <?php
-            if (isset($_GET['stranka']))    //testuju zda jde o prvni zobrazeni, bez kliknuti na menu 
-            {
-               require($_GET['stranka'].".php");
-            
-                    //  @require($_GET['stranka'].".php");//potlačí výpis chyby 
-             }
-             else{
-                  require("uvod.php");
-             
-             }
-          
-  
-          
-          
-            ?>
-    <div class="obsah">
+    <div class="obal">
+      <div class="hlavicka">
+        Toto je hlavička
+      </div> 
+      <!--KONEC HLAVIČKA -->
+      
+      <div class="pas">
+        <div class="levy">
+          <?php  require("menu.php");   ?>
+        </div> <!-- KONEC LEVY-->
+      
+      
+        <?php
+          if (isset($_GET['stranka']))    // Testuju zda jde o prvni zobrazeni, bez kliknutí na menu 
+          {
+            require($_GET['stranka'].".php"); //  @require($_GET['stranka'].".php"); // Potlačí výpis chyby 
+          }
+          else
+          {
+            require("uvod.php");
+          }
+        ?>
+        <div class="obsah">
 
-    </div> <!-- KONEC OBSAH-->
-    </div> <!-- KONEC PAS -->
+        </div> <!-- KONEC OBSAH-->
+      </div> <!-- KONEC PAS -->
 
-    <div class="paticka">
-      Patička
-    </div> <!-- KONEC PATIČKA-->
-  </div> <!-- KONEC OBAL -->
-    
-
-
+      <div class="paticka">
+        Patička
+      </div> <!-- KONEC PATIČKA-->
+    </div> <!-- KONEC OBAL -->
   </body>
 </html>
--- dynamWWW/kontakt.php
+++ dynamWWW/kontakt.php
@@ -1,3 +1,4 @@
-      <h2>Kontakt</h2>
-      <p>Kontakt: <a href="mailto: "webmaster@cokoliv.cz"> webmaster@cokoliv.cz</a>
-      </p>
+<h2>Kontakt</h2>
+<p>
+      Kontakt: <a href="mailto: "webmaster@cokoliv.cz"> webmaster@cokoliv.cz</a>
+</p>
